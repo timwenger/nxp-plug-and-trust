ce392402a3fa84a7e33749ebf55544f8aae6c1b5
Tim Wenger <tim.wenger@outlook.com>
2023-11-06 19:26:02 -0800

    use set() command, rather than file(GLOB) command

--- simw_lib.cmake
+++ simw_lib.cmake
@@ -8,19 +8,23 @@
 
 message("Running simw_lib.cmake: setting variables for libraries, source, and include files ")
 
-file(
-    GLOB
-    SIMW_SE_SOURCES
+set(SIMW_SE_SOURCES
     ${SIMW_LIB_DIR}/sss/ex/src/ex_sss_boot.c
     ${SIMW_LIB_DIR}/sss/ex/src/ex_sss_boot_connectstring.c
     ${SIMW_LIB_DIR}/sss/ex/src/ex_sss_se05x.c
     ${SIMW_LIB_DIR}/sss/ex/src/ex_sss_se05x_auth.c
 
-    ${SIMW_LIB_DIR}/sss/src/*.c
+    ${SIMW_LIB_DIR}/sss/src/fsl_sss_apis.c
+    ${SIMW_LIB_DIR}/sss/src/fsl_sss_util_asn1_der.c
+    ${SIMW_LIB_DIR}/sss/src/fsl_sss_util_rsa_sign_utils.c
     ${SIMW_LIB_DIR}/sss/src/se05x/fsl_sss_se05x_apis.c
     ${SIMW_LIB_DIR}/sss/src/se05x/fsl_sss_se05x_mw.c
     ${SIMW_LIB_DIR}/sss/src/se05x/fsl_sss_se05x_policy.c
-    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/infra/*.c
+    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/infra/global_platf.c
+    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/infra/sm_apdu.c
+    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/infra/sm_connect.c
+    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/infra/sm_errors.c
+    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/infra/sm_printf.c
     ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/log/nxLog.c
     ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/smCom/smCom.c
     ${SIMW_LIB_DIR}/hostlib/hostLib/platform/rsp/se05x_reset.c
@@ -30,7 +34,9 @@ file(
     ${SIMW_LIB_DIR}/hostlib/hostLib/se05x/src/se05x_tlv.c
     ${SIMW_LIB_DIR}/hostlib/hostLib/se05x_03_xx_xx/se05x_APDU.c
     ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/smCom/smComT1oI2C.c
-    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/smCom/T1oI2C/*.c
+    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEse_Api.c
+    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEsePal_i2c.c
+    ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEseProto7816_3.c
     ${SIMW_LIB_DIR}/hostlib/hostLib/platform/linux/i2c_a7.c
     ${SIMW_LIB_DIR}/sss/src/openssl/fsl_sss_openssl_apis.c
     ${SIMW_LIB_DIR}/sss/src/keystore/keystore_cmn.c
@@ -38,25 +44,20 @@ file(
     ${SIMW_LIB_DIR}/sss/src/keystore/keystore_pc.c
 )
 
-file(
-    GLOB
-    SIMW_SE_AUTH_SOURCES
-    ##### needed for authenticated sessions to se05x
+set(SIMW_SE_AUTH_SOURCES
+    # needed for authenticated sessions to se05x
     ${SIMW_LIB_DIR}/sss/ex/src/ex_sss_scp03_auth.c
-
     ${SIMW_LIB_DIR}/sss/src/se05x/fsl_sss_se05x_eckey.c
     ${SIMW_LIB_DIR}/sss/src/se05x/fsl_sss_se05x_scp03.c
     ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/nxScp/nxScp03_Com.c
 )
 
-file(
-    GLOB
-    SIMW_INC_DIR
-    ${SIMW_LIB_DIR}/sss/ex/src
-    ${SIMW_LIB_DIR}/sss/ex/inc
-
+set(COMMON_INC_DIRS
     ${SIMW_LIB_DIR}
     ${SIMW_LIB_DIR}/sss/inc
+    # /sss/ex/inc dir are seemingly includes only needed for example programs, 
+    # but they include type definitions that the iot agent itself uses
+    ${SIMW_LIB_DIR}/sss/ex/inc
     ${SIMW_LIB_DIR}/sss/port/default
     ${SIMW_LIB_DIR}/hostlib/hostLib/inc
     ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/infra
@@ -68,6 +69,13 @@ file(
     ${SIMW_LIB_DIR}/hostlib/hostLib/libCommon/smCom
     ${SIMW_LIB_DIR}/hostlib/hostLib/tstUtil
 )
+
+set(SIMW_INC_DIR
+    ${COMMON_INC_DIRS}
+    ${SIMW_LIB_DIR}/sss/ex/src
+)
+
+
 
 # the "ssl" and "crypto" openssl libraries are needed for several projects
 set(OPENSSL_LIBRARIES ssl crypto)
@@ -84,5 +92,5 @@ add_definitions(-DT1oI2C)
 add_definitions(-DT1oI2C_UM11225)
 add_definitions(-DT1OI2C_RETRY_ON_I2C_FAILED)
 
-# nxp respects this definition for the ex_ecc demo, but not the nxp_iot_agent_demo
+# nxp demo files respect this definition for the ex_ecc demo, but not the nxp_iot_agent_demo
 # add_definitions(-DFLOW_SILENT)
